40d40566f5e78c96da6f03aa68b77e8325e2c208
ISNewton <ashrfalhag.11@gmail.com>
2023-02-03 21:58:16 +0200

    modify orders

--- database/migrations/2023_02_03_195633_create_orders_table.php
+++ database/migrations/2023_02_03_195633_create_orders_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
