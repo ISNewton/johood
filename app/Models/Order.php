962ddb162a23df52349dc718a3fd573ba39bcea1
ISNewton <ashrfalhag.11@gmail.com>
2023-02-04 11:40:59 +0200

    order model realtions

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,11 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
 
     const STATUS_PENDING = 1;
-    
+
+    public function products(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function scopePending($query)
+    {
+        return $query->where('status', self::STATUS_PENDING);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
