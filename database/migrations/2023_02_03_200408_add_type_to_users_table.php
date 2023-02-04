f909e1c10e4df2294edc305b0bcb62b6a682c263
ISNewton <ashrfalhag.11@gmail.com>
2023-02-04 09:04:30 +0200

    student user migration

--- database/migrations/2023_02_03_200408_add_type_to_users_table.php
+++ database/migrations/2023_02_03_200408_add_type_to_users_table.php
@@ -15,11 +15,28 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            //student
             $table->string('type')->nullable()->default(User::TYPE_STUDENT);
             $table->string('gender')->nullable();
             $table->string('first_phone')->nullable();
             $table->string('second_phone')->nullable();
-
+            $table->string('address')->nullable();
+            $table->string('personal_id')->nullable();
+            //guarantor
+            $table->string('guarantor_name')->nullable();
+            $table->string('guarantor_personal_id')->nullable();
+            $table->string('guarantor_job')->nullable();
+            $table->string('guarantor_work_company')->nullable();
+            $table->string('guarantor_work_address')->nullable();
+            $table->string('guarantor_first_phone')->nullable();
+            $table->string('guarantor_second_phone')->nullable();
+            //housing
+            $table->string('house_address')->nullable();
+            $table->string('house_owner')->nullable();
+            $table->string('house_owner_personal_id')->nullable();
+            $table->string('house_owner_gender')->nullable();
+            $table->string('house_owner_phone')->nullable();
+            $table->string('house_number')->nullable();
         });
     }
 
