972be13d2af46215622cfa70a558e7534ac28a7a
ISNewton <ashrfalhag.11@gmail.com>
2023-02-05 19:15:24 +0200

    init payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'payment_date',
+        'payment_status',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
