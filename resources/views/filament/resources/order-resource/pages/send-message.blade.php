b1dbc991e599e1fc7da16586797478df21017519
ISNewton <ashrfalhag.11@gmail.com>
2023-02-06 08:23:23 +0200

    send message custome page

--- resources/views/filament/resources/order-resource/pages/send-message.blade.php
+++ resources/views/filament/resources/order-resource/pages/send-message.blade.php
@@ -1,3 +1,10 @@
 <x-filament::page>
-
+  <input type="text"
+    class="block w-25 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500 border-gray-300 dark:border-gray-600"
+    x-bind:class="{
+  'border-gray-300': ! ('data.price' in $wire.__instance.serverMemo.errors),
+  'dark:border-gray-600': ! ('data.price' in $wire.__instance.serverMemo.errors) &amp;&amp; true,
+  'border-danger-600 ring-danger-600': ('data.price' in $wire.__instance.serverMemo.errors),
+  'dark:border-danger-400 dark:ring-danger-400': ('data.price' in $wire.__instance.serverMemo.errors) &amp;&amp; true,
+}">
 </x-filament::page>
