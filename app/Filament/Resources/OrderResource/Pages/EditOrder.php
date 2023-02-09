78d3e48077d04774ea9d520bb64f3fb47c46608e
ISNewton <ashrfalhag.11@gmail.com>
2023-02-09 17:39:51 +0200

    add rich editor to send message

--- app/Filament/Resources/OrderResource/Pages/EditOrder.php
+++ app/Filament/Resources/OrderResource/Pages/EditOrder.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Resources\OrderResource\Pages;
 
-use App\Filament\Resources\OrderResource;
 use Filament\Pages\Actions;
+use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\OrderResource;
 
 class EditOrder extends EditRecord
 {
@@ -16,4 +17,26 @@ class EditOrder extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    
+
+    // public function do()
+    // {
+    //     // dd(555);
+    //     // return SendMessage::make()
+    //     //     ->record($this->record)
+    //     //     ->title('Send Message')
+    //     //     ->modalSize('sm')
+    //     //     ->showCloseButton(true)
+    //     //     ->showCancelButton(true)
+    //     //     ->showConfirmButton(true)
+    //     //     ->confirmButtonText('Send')
+    //     //     ->cancelButtonText('Cancel')
+    //     //     ->confirmButtonColor('blue')
+    //     //     ->cancelButtonColor('gray')
+    //     //     ->confirmButtonLoadingText('Sending...')
+    //     //     ->cancelButtonLoadingText('Canceling...')
+    //     //     ->onConfirm(fn () => $this->sendMessage())
+    //     //     ->onCancel(fn () => $this->dismissModal());
+    // }
 }
--- app/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -2,11 +2,60 @@
 
 namespace App\Filament\Resources\OrderResource\Pages;
 
-use App\Filament\Resources\OrderResource;
+use App\Models\User;
 use Filament\Pages\Actions;
+use Filament\Pages\Actions\Action;
+use Filament\Forms\Components\Select;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Resources\Pages\ViewRecord;
+use App\Filament\Resources\OrderResource;
+use Filament\Forms\Components\RichEditor;
 
 class ViewOrder extends ViewRecord
 {
     protected static string $resource = OrderResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Action::make('send-message')
+                ->label('ارسال رسالة')
+                ->icon('heroicon-o-chat-alt-2')
+                ->action('do')
+                ->form([
+                    Select::make('phone')
+                        ->label('رقم الهاتف')
+                        ->options(fn () => [
+                            $this->record->user->first_phone => $this->record->user->first_phone, $this->record->user->second_phone => $this->record->user->second_phone
+                        ])
+                        ->required(),
+                    RichEditor::make('description')
+                    ->label('محتوى الرسالة')
+                    ->required()
+                    ->columnSpanFull(3),
+
+                ]),
+        ];
+    }
+
+    public function do($data)
+    {
+        dd($data, $this->record);
+
+        // return SendMessage::make()
+        //     ->record($this->record)
+        //     ->title('Send Message')
+        //     ->modalSize('sm')
+        //     ->showCloseButton(true)
+        //     ->showCancelButton(true)
+        //     ->showConfirmButton(true)
+        //     ->confirmButtonText('Send')
+        //     ->cancelButtonText('Cancel')
+        //     ->confirmButtonColor('blue')
+        //     ->cancelButtonColor('gray')
+        //     ->confirmButtonLoadingText('Sending...')
+        //     ->cancelButtonLoadingText('Canceling...')
+        //     ->onConfirm(fn () => $this->sendMessage())
+        //     ->onCancel(fn () => $this->dismissModal());
+    }
 }
