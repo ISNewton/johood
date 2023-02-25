b9a6b1dbbd99c3ce4f0e0df40225d804b0163cff
Ashraf Alhaj <ashrfalhag.11@gmail.com>
2023-02-25 19:03:56 +0200

    projects list in products page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,28 +10,7 @@
               <p class="testimonial_text">التحول الرقمي</p>
               <div class="testimonial_section_2">
                  <div class="row">
-                    <div class="col-md-6">
-                       <div class="testimonial_box">
-                          <div class="jonimo_taital_main">
-                             <h4 class="jonimo_text">المشروع 1</h4>
-                             <div class="quick_icon"><img src="images/quick-icon.png"></div>
-                             <div class="quick_icon_1"><img src="images/quick-icon1.png"></div>
-                          </div>
-                          <p class="dummy_text">برنامج جهود القومي لتطوير الادارة الاليكترونية</p>
-                          <div class="read_bt" style="margin-right:150px;"><a href="blog.html">ديجي شباب</a></div>
-                       </div>
-                    </div>
-                    <div class="col-md-6">
-                       <div class="testimonial_box">
-                          <div class="jonimo_taital_main">
-                             <h4 class="jonimo_text">المشروع 2</h4>
-                             <div class="quick_icon"><img src="images/quick-icon.png"></div>
-                             <div class="quick_icon_1"><img src="images/quick-icon1.png"></div>
-                          </div>
-                          <p class="dummy_text">برنامج جهود القومي لتطوير الادارة الاليكترونية</p>
-                          <div class="read_bt" style="margin-right:150px;"><a href="DCG.html">DCG</a></div>
-                       </div>
-                    </div>
+                    @livewire('projects-list')
                  </div>
               </div>
            </div>
